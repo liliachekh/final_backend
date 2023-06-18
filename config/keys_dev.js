a8af3f59b7d338ce03405a04cd37f180d7321e59
Lilia Chekh <liliacheh@gmail.com>
2023-06-18 18:23:17 +0200

    changed to default settings

--- config/keys_dev.js
+++ config/keys_dev.js
@@ -1,15 +1,5 @@
 module.exports = {
   mongoURI:
-    process.env.MONGO_URI,
+    'mongodb+srv://liliacheh:vW4PV1ZWCcpHSybR@cluster0.squljoe.mongodb.net/?retryWrites=true&w=majority',
   secretOrKey: 'random very very secret string',
 };
-
-// module.exports = {
-//   mongoURI: 'mongodb+srv://liliacheh:vW4PV1ZWCcpHSybR@cluster0.squljoe.mongodb.net/?retryWrites=true&w=majority',
-//   secretOrKey: 'random very very secret string',
-//   email: {
-//     mailUser: 'crypter.mailer@gmail.com',
-//     mailPassword: 'V1kfuhtC',
-//     mailService: 'gmail'
-//   }
-// };
--- server.js
+++ server.js
@@ -80,4 +80,3 @@ if (process.env.NODE_ENV === 'production') {
 const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-// module.exports = app;
