4e3c77109ce93c845c9102414f71f38d8432ecf0
Lilia Chekh <liliacheh@gmail.com>
2023-06-20 12:41:16 +0200

    changed server.js

--- server.js
+++ server.js
@@ -76,5 +76,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+const port = process.env.PORT || 4000;
 
-module.exports = app;
+app.listen(port, () => console.log(`Server running on port ${port}`));
+
