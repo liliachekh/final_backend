1411baabbf86ea148b84b5ef62280a1d62278715
Lilia Chekh <liliacheh@gmail.com>
2023-06-24 21:38:02 +0200

    turn off jwt

--- routes/products.js
+++ routes/products.js
@@ -56,7 +56,7 @@ const upload = multer({
 // @access  Private
 router.post(
   "/images",
-  passport.authenticate("jwt-admin", { session: false }),
+  // passport.authenticate("jwt-admin", { session: false }),
   upload.array("photos"),
   addImages
 );
@@ -66,7 +66,7 @@ router.post(
 // @access  Private
 router.post(
   "/",
-  passport.authenticate("jwt-admin", { session: false }),
+  // passport.authenticate("jwt-admin", { session: false }),
   addProduct
 );
 
@@ -75,7 +75,7 @@ router.post(
 // @access  Private
 router.put(
   "/:id",
-  passport.authenticate("jwt-admin", { session: false }),
+  // passport.authenticate("jwt-admin", { session: false }),
   updateProduct
 );
 
