81d4416357f03f74dff029e1c1fd31a318e2f87f
Lilia Chekh <liliacheh@gmail.com>
2023-07-12 20:04:06 +0200

    changed app.get('*', (req, res)

--- server.js
+++ server.js
@@ -80,7 +80,8 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'static')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    // res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendStatus(404)
   });
 }
 const port = process.env.PORT || 4000;
