c2169878d83fbec1d6bf3d588c88cc4560b0b797
Lilia Chekh <liliacheh@gmail.com>
2023-07-12 19:53:44 +0200

    Added path.join(__dirname, 'static')

--- server.js
+++ server.js
@@ -77,6 +77,7 @@ app.use('/api/partners', partners);
 if (process.env.NODE_ENV === 'production') {
   // Set static folder
   app.use(express.static('client/build'));
+  app.use(express.static(path.join(__dirname, 'static')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
